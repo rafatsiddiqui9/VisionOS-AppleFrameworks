c936e9ec404feee1322a4ad0b18dd7f596492f89
Rafat Siddiqui <mdrafat.siddiqui@outlook.com>
2024-02-06 19:42:08 +0530

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,21 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Rafat on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
